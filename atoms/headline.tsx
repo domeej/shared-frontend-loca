b2ed6fd972b0af00200f833f411faf8ec498f0e8
Domenik Köhli <domenik.koehli@aoe.com>
2022-06-07 13:49:04 +0200

    Revert "ts than js"
    This reverts commit 1e52546ae998d356719bfebcd2f4277bc5a1242b.

--- atoms/headline.tsx
+++ atoms/headline.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Headline() :  React.ReactElement {
+export default function Headline() {
     return (
         <p> This is a Paragraph Component</p>
     )
